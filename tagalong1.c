cde091866fcf57174d42aee664426f638bea3473
trinitywarner <trinity.warner@bsu.edu>
2026-01-12 19:42:39 +0000

    Video 1 of 6 complete.

---
+++ tagalong1.c
@@ -0,0 +1,22 @@
+// this means a comment
+// another demmenstration, c code
+
+#include <stdio.h>
+// stdio.h is a library of standard input and output 
+// including a library is to use <>, as shown above
+
+// this begins the function, main, this is great for organizing and is a necessity to do most things
+// function has a return type
+// {} indicates starting and ending blocks (beginning and end) 
+// () is there to set parameters, not neccessary entirely
+// printf is c's way of saying print format
+// \n means new line
+// ; this means to end a line, like normal punctuation
+// return means to return an error code-- the function, but if there are no errors, just return a 0 
+
+int main(){
+	printf("Hello World! from me! \n");
+	return(0);
+}
+
+
