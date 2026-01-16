fb9d0755f2fd7520b0e72c9056c72d29d7a10ccb
trinitywarner <trinity.warner@bsu.edu>
2026-01-16 19:35:38 +0000

    C string functions, branches, and loops.

--- while.c
+++ while.c
@@ -4,11 +4,18 @@
 int main(){
 	char response[10];
 	strcpy(response, "no");
+	int keepGoing = 1;
 
 //only a condition goes here v
-	while(strcmp(response, "yes") != 0){
+//	while(strcmp(response, "yes") != 0){
+//	while keepGoing indicates that if the answer is no, the loop will keep going, asking the same question until it gets the desired answer, AKA, 'yes'
+	While(keepGoing){
 		printf("Are we there yet? ");
 		scanf("%s", response);
+		if (strcmp(reponse, "yes") == 0){
+			// if they answer yes, this is where it will end, with keepGoing setting to 0 so it does not repeat. Ending with "Can we go home now?"
+			keepGoing = 0;
+		} // end if
 	} // end while
 	printf("Can we go home now?" \n");
 
