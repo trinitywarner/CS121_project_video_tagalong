76510f4c4d295cbf65ca2b98ca775046ac5ab4c0
trinitywarner <trinity.warner@bsu.edu>
2026-01-16 19:53:13 +0000

    C string functions, braches, and loops.

---
+++ random.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+	srand(time(NULL));
+	int a;
+	for (int i = 0; < 10; i++){
+		a = rand();
+		a (a % 6) + 1;
+		printf("%d ", a);
+	} // end for
+	print f("\n");
+
+	return 0;
+} // end main
+
+
